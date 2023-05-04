17c0fc25a19a159418419372142fc822205c60a0
iamrealleader <sabircg125@gmail.com>
2023-05-04 17:15:28 +0500

    fixing some bugs

--- connectDb.js
+++ connectDb.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connect = mongoose.connect(process.env.LOCAL_URL, {
+const connect = mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
